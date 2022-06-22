c5ee8aed5bfacc929f52de16c224151b0ba82845
Tomáš Liška <76174508+kontridio@users.noreply.github.com>
2022-06-22 23:09:01 +0200

    2. backend

---
+++ backend.2/src/api/zanr/controllers/zanr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  zanr controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::zanr.zanr');
---
+++ backend.2/src/api/zanr/routes/zanr.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * zanr router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::zanr.zanr');
